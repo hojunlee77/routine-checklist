cad2ab4f45e92a980c8322ef3745f26bc623dab2
hojunlee77 <54741551+hojunlee77@users.noreply.github.com>
2020-05-03 01:33:13 +0900

    initial

--- App.js
+++ App.js
@@ -1,19 +1,44 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { AppLoading } from "expo";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  state = {
+    loadedToDos: false,
+  };
+
+  componentDidMount = () => {
+    this._loadToDos();
+  };
+
+  render() {
+    const { loadedToDos } = this.state;
+    if (!loadedToDos) {
+      return <AppLoading />;
+    }
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>HiHi</Text>
+      </View>
+    );
+  }
+  _loadToDos = () => {
+    this.setState({
+      loadedToDos: true,
+    });
+  };
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+  },
+  title: {
+    color: "black",
+    fontSize: 50,
+    marginTop: 70,
+    fontWeight: "200",
   },
 });
--- README.md
+++ README.md
@@ -1,6 +1,25 @@
 # routine-checklist
+
 Practicing React-Native
 
+## Screen
 
-- [ ] create Category
-- [ ]
+- [ ] Home
+  - [ ] Category List
+  - [ ] Add Category
+  - [ ] Edit Category
+- [ ] In the Category
+
+## Category
+
+- [ ] Create Category
+- [ ] Delete Category
+- [ ] Edit Category
+
+## ToDo-List
+
+- [ ] Create a ToDo List
+- [ ] Delete a ToDo List
+- [ ] Edit a ToDo List
+
+- [ ] Organize Items
